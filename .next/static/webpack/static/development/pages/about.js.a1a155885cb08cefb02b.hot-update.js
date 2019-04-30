f2030ffa14b251eda1a63a9e643c343932b2e042
Philliph Drummond <oilsinwater@gmail.com>
2019-04-30 13:44:42 -0700

    Initializes reposity

---
+++ .next/static/webpack/static/development/pages/about.js.a1a155885cb08cefb02b.hot-update.js
@@ -0,0 +1,30 @@
+webpackHotUpdate("static/development/pages/about.js",{
+
+/***/ "./pages/about.js":
+/*!************************!*\
+  !*** ./pages/about.js ***!
+  \************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/phil/dev/next-crash-course/pages/about.js";
+
+
+var About = function About() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  });
+};
+
+/***/ })
+
+})
+//# sourceMappingURL=about.js.a1a155885cb08cefb02b.hot-update.js.map
---
+++ .next/static/webpack/static/development/pages/about.js.f77bd9d7e19bf800af52.hot-update.js
@@ -0,0 +1,36 @@
+webpackHotUpdate("static/development/pages/about.js",{
+
+/***/ "./pages/about.js":
+/*!************************!*\
+  !*** ./pages/about.js ***!
+  \************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/phil/dev/next-crash-course/pages/about.js";
+
+
+var About = function About() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "About BitzPrice"));
+};
+
+/***/ })
+
+})
+//# sourceMappingURL=about.js.f77bd9d7e19bf800af52.hot-update.js.map
